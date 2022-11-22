ec8c5639b249f21a1d7dcc66b57a2f3af9a7caf1
krbtsv <maxim.karabtsov@gmail.com>
2022-11-22 22:37:30 +0300

    Created view for index page, product page

---
+++ store/products/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from products.views import index, products
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('products/', products, name='products')
+]
