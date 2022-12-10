31193891d65b063933945fbe88597fb2296b6c30
krbtsv <maxim.karabtsov@gmail.com>
2022-12-10 05:49:57 +0300

    Added order-create template.

--- store/orders/views.py
+++ store/orders/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class OrderCreateView(TemplateView):
+    template_name = 'orders/order-create.html'
