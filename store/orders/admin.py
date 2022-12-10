667ba66f4cd9cea0e10c8d04c7d6bcb0ecc6c5e0
krbtsv <maxim.karabtsov@gmail.com>
2022-12-10 06:25:47 +0300

    Created Order model. registered OrderAdmin

--- store/orders/admin.py
+++ store/orders/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from orders.models import Order
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'email', 'address', 'basket_history', 'created', 'status', 'initiator')
+    list_display_links = ('id', )
