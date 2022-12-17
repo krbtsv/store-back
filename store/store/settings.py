9b5b67880f902173b3d15077fd9f7db7ed5f87dc
krbtsv <maxim.karabtsov@gmail.com>
2022-12-17 04:12:33 +0300

    Created STATIC_ROOT variable for deploy

--- store/store/settings.py
+++ store/store/settings.py
@@ -187,9 +187,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    BASE_DIR / 'static',
-)
+
+if DEBUG:
+    STATICFILES_DIRS = (
+        BASE_DIR / 'static',
+    )
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
