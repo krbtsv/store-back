936d07e9434fc6dc623d1a4682baa0b2be34439e
krbtsv <maxim.karabtsov@gmail.com>
2022-12-18 17:29:41 +0300

    [upd] for deploy

--- store/store/settings.py
+++ store/store/settings.py
@@ -190,10 +190,10 @@ STATIC_URL = '/static/'
 
 if DEBUG:
     STATICFILES_DIRS = (
-        BASE_DIR / 'static',
+        os.path.join(BASE_DIR, 'static'),
     )
 else:
-    STATIC_ROOT = BASE_DIR / 'static'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
