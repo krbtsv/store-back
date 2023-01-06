e81ead1ab69e90ac2e584114336c14d3ba403c35
krbtsv <maxim.karabtsov@gmail.com>
2023-01-06 23:26:56 +0300

    created test for get ProductAPI

--- store/products/tests/test_api.py
+++ store/products/tests/test_api.py
@@ -1,14 +1,18 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
-from products.models import ProductCategory
-from products.serializers import ProductCategorySerializer
+from products.models import ProductCategory, Product
+from products.serializers import ProductCategorySerializer, ProductSerializer
 
 
-class ProductCategoryAPITestCase(APITestCase):
+class ProductsAPITestCase(APITestCase):
     def setUp(self):
         ProductCategory.objects.create(name='Одежда')
         ProductCategory.objects.create(name='Обувь')
         ProductCategory.objects.create(name='Аксессуры')
+
+        Product.objects.create(name='Ботинок', description='Прочный', price=100, quantity=5, category_id=2)
+        Product.objects.create(name='Пальто', description='Шикарное', price=150, quantity=7, category_id=1)
+        Product.objects.create(name='Сумка', description='Слезы крокодила', price=200, quantity=15, category_id=3)
 
     def test_get_product_category(self):
         url = 'http://127.0.0.1:8000/store_dress_api/categories/'
@@ -16,5 +20,14 @@ class ProductCategoryAPITestCase(APITestCase):
         categories = ProductCategory.objects.all()
 
         serializer_data = ProductCategorySerializer(categories, many=True).data
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data['results'])
+
+    def test_get_products(self):
+        url = 'http://127.0.0.1:8000/store_dress_api/products/'
+        response = self.client.get(url)
+        products = Product.objects.all()
+
+        serializer_data = ProductSerializer(products, many=True).data
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data['results'])
